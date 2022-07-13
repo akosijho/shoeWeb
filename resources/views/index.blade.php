d2bdcfbf456353691f4fddee339766c859b222bf
jcrocker-cmd <narbajajc@gmail.com>
2022-07-13 15:22:54 +0800

    Fix Logo Position

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.master')
 @section('content')
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <img src="Images/app_logo.png" class="logo">
-            <ul>
-				<li><a class="home" href="index.html">Home</a></li>
+<nav class="navbar navbar-light ">
+  <div class="container-fluid d-flex flex-row-reverse container">
+    <img src="Images/app_logo.png" class="logo img-fluid">
+            <ul >
+				<li><a href="index.html" class="home">Home</a></li>
 				<li><a href="">Feature</a></li>
 				<li><a href="">How it Works</a></li>
 				<li><a href="">About</a></li>
