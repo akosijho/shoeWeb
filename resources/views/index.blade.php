514819171be9085343c269f7c0312f5113176caa
root <root@DESKTOP-J5PV2UC.localdomain>
2022-07-13 13:59:50 +0800

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+@section('content')
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <img src="Images/app_logo.png" class="logo">
+            <ul>
+				<li><a class="home" href="index.html">Home</a></li>
+				<li><a href="">Feature</a></li>
+				<li><a href="">How it Works</a></li>
+				<li><a href="">About</a></li>
+				<li><a href="">Contact us</a></li>
+			</ul>
+  </div>
+</nav>
+
+
+@endsection
