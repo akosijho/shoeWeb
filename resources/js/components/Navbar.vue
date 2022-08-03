232621de9a0198f36571b47edafe65061d9913ce
root <root@DESKTOP-CPT0F7L.localdomain>
2022-08-03 07:23:29 +0100

    Added Video in about page.

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+
+<nav>
+        <div class="logo">
+        
+        </div>
+        
+        <ul>
+                <li> <a href="#home">Home</a></li>
+				<li><a href="#features">Features</a></li>
+				<li><a href="#howitworks">How it Works</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="#contact">Contact us</a></li>
+        </ul>
+
+        <div class="input">
+            <input type="text" name="" id="sbox" placeholder="Search">
+        </div>
+</nav>
+  
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+
+}
+</script>
+
+<style>
+
+@import "./resources/css/style.css";
+
+</style>
--- resources/views/layout/home/about.blade.php
+++ resources/views/layout/home/about.blade.php
@@ -19,58 +19,68 @@
             love to build products that people love to use! With our team’s diverse skill set – user experience,
              design, branding, web and mobile development, product strategy and business analysis – Symph is
               constantly turning our customer’s ideas into successful realities.</p>
-                                </div>
+                                </div> 
 
     </div>
 
 
-    <div class = "teamrow">
+    
+    <div class="teamtitle_row">
         <div class="teamtitle">
             <h1>Our Team</h1>
             <p>The awesome people behind amazing things.</p>
 
         </div>
 
-        
+        </div>
 
-
-    </div>
+    
     
     <div class="teamrow_1">
-            
+    
+    
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/aw.png" alt="" class="baby">
+                        <video width="328" height="227" class="baby" muted loop autoplay>
+                        <source src="Video/jhobert.mp4" type="video/mp4"  muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
                 <h2>Jhobert Panerio</h2>
-                <p>Founder + CEO</p>
+                <p>Mobile Developer</p>
             </div>
         </div>
 
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/aw.png" alt="" class="baby">
+                        <video width="328" height="227"  class="baby"  muted loop autoplay>
+                        <source src="Video/pansang.mp4" type="video/mp4"muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
                 <h2>Maria Jessa E. Camposo</h2>
-                <p>Founder + CEO</p>
+                <p>Mobile Developer</p>
             </div>
         </div>
 
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/aw.png" alt="" class="baby">
+                        <video width="328" height="227"class="baby" muted loop autoplay>
+                        <source src="Video/ilven.mp4" type="video/mp4"  muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
-                <h2>Ilven Brix Brina</h2>
-                <p>Founder + CEO</p>
+                <h2>Ilven Rex Brina</h2>
+                <p>Mobile Developer</p>
             </div>
         </div>
 
@@ -83,36 +93,45 @@
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/aw.png" alt="" class="baby">
+                        <video width="328" height="227" class="baby" muted loop autoplay>
+                        <source src="Video/gadon.mp4" type="video/mp4"  muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
                 <h2>John Christian D. Gadon</h2>
-                <p>Founder + CEO</p>
+                <p>Mobile Developer</p>
             </div>
         </div>
 
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/aw.png" alt="" class="baby">
+                        <video width="328" height="227" class="baby"  muted loop autoplay>
+                        <source src="Video/baral.mp4" type="video/mp4" muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
                 <h2>Mark Russel T. Baral</h2>
-                <p>Founder + CEO</p>
+                <p>Web Developer</p>
             </div>
         </div>
 
         <div class="box">
             <div class="boximage">
                 <div class="faceid">
-                    <img src="Images/john.png" alt="" class="baby">
+                        <video width="328" height="227" class="baby" muted loop autoplay>
+                        <source src="Video/ako.mp4" type="video/mp4"  muted loop autoplay>
+                         
+                        </video>
                 </div>
            
             <div class ="about_content"></div>
                 <h2>John Christian B. Narbaja</h2>
-                <p>Founder + CEO</p>
+                <p>Web Developer</p>
             </div>
         </div>
 
